85f70a6c63b7f8a3aed569f3e421a96a1447c315
davisantosr <davi-rocha2@hotmail.com>
2020-12-22 15:36:12 -0300

    state

--- App.js
+++ App.js
@@ -1,38 +1,36 @@
 import React, { Component } from 'react';
 
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Button } from 'react-native';
 
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props)
 
-    let nome = 'Davi Santos'
-    return (
-      <View>
-        <Text style={{color:'white', backgroundColor:'blue'}}>
-          {nome}
-        </Text>
-        <Text style={{fontWeight:'bold'}}>Software Developer</Text>
-        <Jobs altura={200} largura={100} nome={'Steve Jobs'}/>
-          
-      </View>
-    )
+    this.state = {
+      nome: ''
+    }
+
+    this.entrar = this.entrar.bind(this)
   }
-}
 
-class Jobs extends Component {
+
+  entrar(nome) {
+    this.setState({
+      nome
+    })
+  }
+
   render() {
     return (
-      <View>
-        <Image 
-          source={{uri: `https://images.unsplash.com/
-            photo-1593642634443-44adaa06623a?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=
-            rb-1.2.1&auto=format&fit=crop&w=1525&q=80` }}
-          style={{width: this.props.largura, height: this.props.altura}} 
-        />
-        <Text> {this.props.nome} </Text>
+      <View style={{marginTop: 20}}>
+        <Text style={{fontSize: 100, color: 'blue', textAlign: 'center'}}>
+          {this.state.nome}
+        </Text>
+        <Button 
+          title='button'
+          onPress={() => this.entrar('Santos')} />
       </View>
-      
-      
+    
     )
   }
 }
