7c6cc837e3125fde28bbf4290e90ecd532a703fe
davisantosr <davi-rocha2@hotmail.com>
2020-12-22 15:09:45 -0300

    add component|props

--- App.js
+++ App.js
@@ -6,22 +6,33 @@ class App extends Component {
   render() {
 
     let nome = 'Davi Santos'
-    let image = `https://images.unsplash.com/
-      photo-1593642634443-44adaa06623a?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=
-      rb-1.2.1&auto=format&fit=crop&w=1525&q=80`
     return (
       <View>
         <Text style={{color:'white', backgroundColor:'blue'}}>
           {nome}
         </Text>
         <Text style={{fontWeight:'bold'}}>Software Developer</Text>
-        {/* <Image 
-          source={{uri:image}}
-          style={{width: '30'}}
-          
-        /> */}
+        <Jobs altura={200} largura={100} nome={'Steve Jobs'}/>
           
       </View>
+    )
+  }
+}
+
+class Jobs extends Component {
+  render() {
+    return (
+      <View>
+        <Image 
+          source={{uri: `https://images.unsplash.com/
+            photo-1593642634443-44adaa06623a?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=
+            rb-1.2.1&auto=format&fit=crop&w=1525&q=80` }}
+          style={{width: this.props.largura, height: this.props.altura}} 
+        />
+        <Text> {this.props.nome} </Text>
+      </View>
+      
+      
     )
   }
 }
