e6c8433b8c8489b2fa8c4236e1357774f365e83f
davisantosr <davi-rocha2@hotmail.com>
2021-01-03 18:58:27 -0300

    FlatList | props

--- myApp/App.js
+++ myApp/App.js
@@ -3,11 +3,23 @@ import React, { Component } from 'react';
 import { 
   View, 
   StyleSheet, 
-  FlatList
+  FlatList,
+  Text
 
 } from 'react-native';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      feed: [
+        {id: 1, nome:'Davi', idade: 100, email: 'email.com'},
+        {id: 2,nome:'Joao', idade: 200, email: 'email.com'},
+        {id: 3,nome:'Jose', idade: 400, email: 'email.com'},
+        {id: 4,nome:'Salomao', idade: 500, email: 'email.com'},
+      ]
+    }
+  }
 
   
 
@@ -15,8 +27,9 @@ class App extends Component {
     return (
       <View style={styles.container}>
         <FlatList 
-          data={}
-          renderItem={}
+          data={this.state.feed}
+          keyExtractor={(item) => item.id}
+          renderItem={({item}) => <Pessoa data={item} />}
         />
       </View>     
     )
@@ -31,3 +44,24 @@ const styles = StyleSheet.create({
 })
 
 export default App;
+
+
+class Pessoa extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text style={styles2.textPessoa}>{this.props.data.nome}</Text>
+        <Text style={styles2.textPessoa}>{this.props.data.idade}</Text>
+        <Text style={styles2.textPessoa}>{this.props.data.email}</Text>
+      </View>
+    )
+  }
+}
+
+const styles2 = StyleSheet.create({
+  textPessoa: {
+    backgroundColor: '#222',
+    height: 200,
+    marginBottom: 15
+  }
+})
