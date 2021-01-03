868819216995943bfe957a47ff86163d91fbc3af
davisantosr <davi-rocha2@hotmail.com>
2021-01-03 16:26:56 -0300

    fortune cake| add styling

--- fortuneCakeApp/App.js
+++ fortuneCakeApp/App.js
@@ -37,11 +37,39 @@ class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   img: {
     width: 250, 
     height: 250
   },
+  textAbout: {
+    fontSize: 20,
+    color: '#dd7b22',
+    margin: 30, 
+    fontStyle: 'italic',
+    textAlign: 'center'
+  },
+  btn: {
+    width: 230, 
+    height: 50, 
+    borderWidth: 2, 
+    borderColor: '#dd7b22',
+    borderRadius: 25
+  },
+  btnArea: {
+    flex: 1, 
+    flexDirection: 'row',
+    justifyContent: 'center', 
+    alignItems: 'center'
+  },
+  btnText: {
+    fontSize: 18, 
+    fontWeight: 'bold',
+    color: '#dd7b22',
+
+  }
 })
 
 export default App
