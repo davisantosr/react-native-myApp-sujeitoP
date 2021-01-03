396795e324bd6ba2d81bf1ea958226ccea3515ed
davisantosr <davi-rocha2@hotmail.com>
2021-01-03 17:36:14 -0300

    chronometer app| add styling

--- chronometerApp/App.js
+++ chronometerApp/App.js
@@ -1,114 +1,75 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React from 'react'
 import {
-  SafeAreaView,
+  View, 
+  Text, 
   StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+  TouchableOpacity,
+  Image
+} from 'react-native'
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image 
+          source={require('./chronometer.png')}
+          style={styles.imgChrono}
+        />
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+        <Text style={styles.timer}>0.0</Text>
+
+
+        <View style={styles.btnsArea}>
+          <TouchableOpacity style={styles.btn}>
+            <Text style={styles.btnText}>Go</Text>            
+          </TouchableOpacity>
+          
+          <TouchableOpacity style={styles.btn}>
+            <Text style={styles.btnText}>Stop</Text>
+          </TouchableOpacity>
+        </View>
+
+
+
+      </View>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#00aeef'
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  timer: {
+    marginTop: -160,
+    color: '#fff',
+    fontSize: 65,
+    fontWeight: 'bold'
   },
-  body: {
-    backgroundColor: Colors.white,
+  btnsArea: {
+    flexDirection: 'row',
+    marginTop: 80,
+    height: 40,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+  btn: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center', 
+    backgroundColor: '#fff',
+    height: 40, 
+    margin: 17,
+    borderRadius: 9
 
-export default App;
+  },
+  btnText: {
+    fontSize: 20, 
+    fontWeight: 'bold',
+    color: '#00aeef'
+
+  }
+})
+
+export default App
