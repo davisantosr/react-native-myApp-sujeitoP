0118e1dff4188f742de5df823e715518a5a73414
davisantosr <davi-rocha2@hotmail.com>
2021-01-04 20:25:42 -0300

    Switch| boilerplate

--- myApp/App.js
+++ myApp/App.js
@@ -1,47 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { 
-  View,
-  Text,
+  View, 
+  Text, 
   StyleSheet,
+  Switch 
 } from 'react-native'
 
-import Slider from '@react-native-community/slider'
 
-export class App extends Component {
-  constructor(props) {
-    super(props)
+class App extends React.Component {
+  constructor() {
+    super()
 
     this.state = {
-      value: 50
+
     }
   }
   render() {
-
     return (
-      <View style={styles.container}>
-        <Slider
-          minimumValue={0}
-          maximumValue={100}
-          value={50}
-          onValueChange={selected => {
-            this.setState({
-              value: selected.toFixed(0)
-            })
-          }}
-          minimumTrackTintColor='#00ff00'
-          maximumTrackTintColor='#ff0000'
-        />
-        <Text>{this.state.value}</Text>
+      <View>
+        
       </View>
-    )
+    ) 
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 20
-  }
-})
 
 export default App
