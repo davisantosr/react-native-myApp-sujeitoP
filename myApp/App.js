27d31f728f25a088fbebf8b5c062a003b7dcd64a
davisantosr <davi-rocha2@hotmail.com>
2021-01-05 16:29:43 -0300

    Modal | props

--- myApp/App.js
+++ myApp/App.js
@@ -3,6 +3,8 @@ import {
   View, 
   Text, 
   StyleSheet,
+  Button,
+  Modal,
 
 } from 'react-native'
 
@@ -11,13 +13,31 @@ export class App extends Component {
     super(props)
 
     this.state = {
+      modalVisible: false
     }
+
+    this.handleBtnClick = this.handleBtnClick.bind(this)
   }
 
+  handleBtnClick() {
+    this.setState({
+      modalVisible: !this.state.modalVisible
+    })
+  }
+ 
   render() {
     return (
       <View style={styles.container}>
-       
+        <Button title={'Enter'} onPress={this.handleBtnClick} />
+       <Modal
+        animationType={'slide'}
+        visible={this.state.modalVisible}
+       >
+         <View style={{backgroundColor: '#292929', flex: 1}}>
+           <Text style={{color: '#fff', fontSize: 28}}>Welcome!</Text>
+           <Button title={'Exit'} onPress={this.handleBtnClick} />
+         </View>
+       </Modal>
       </View>
     )
   }
