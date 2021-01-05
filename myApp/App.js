ccd3f1064c8e3516429247c16f4e1055d2b3bf58
davisantosr <davi-rocha2@hotmail.com>
2021-01-05 16:52:21 -0300

    http requests| add api request | add FlatList

--- myApp/App.js
+++ myApp/App.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, FlatList } from 'react-native'
 
 import api from './src/services/api'
 
 export class App extends Component {
+  constructor(props) {
+    super(props) 
+
+      this.state = {
+        filmes: []
+      }
+  }
+
+  async componentDidMount() {
+    const response = await api.get('/r-api/?api=filmes')
+    this.setState({
+      filmes: response.data
+    })
+  }
+
   render() {
     return (
-      <View>
+      <View style={styles.container}>
+        <FlatList 
+          data={this.state.filmes}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({item }) => <Filme data={item} /> }
 
+        />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
 
 export default App
