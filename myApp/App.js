ae8d9b41c5a9df0faad20c0615dfadb16f614f8d
davisantosr <davi-rocha2@hotmail.com>
2021-01-05 16:44:32 -0300

    http requests| add axios | add baseUrl

--- myApp/App.js
+++ myApp/App.js
@@ -1,56 +1,16 @@
 import React, { Component } from 'react'
-import { 
-  View, 
-  Text, 
-  StyleSheet,
-  Button,
-  Modal,
+import { View, StyleSheet } from 'react-native'
 
-} from 'react-native'
+import api from './src/services/api'
 
 export class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      modalVisible: false
-    }
-
-    this.handleBtnClick = this.handleBtnClick.bind(this)
-  }
-
-  handleBtnClick() {
-    this.setState({
-      modalVisible: !this.state.modalVisible
-    })
-  }
- 
   render() {
     return (
-      <View style={styles.container}>
-        <Button title={'Enter'} onPress={this.handleBtnClick} />
-       <Modal
-        animationType={'slide'}
-        visible={this.state.modalVisible}
-       >
-         <View style={{backgroundColor: '#292929', flex: 1}}>
-           <Text style={{color: '#fff', fontSize: 28}}>Welcome!</Text>
-           <Button title={'Exit'} onPress={this.handleBtnClick} />
-         </View>
-       </Modal>
+      <View>
+
       </View>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center', 
-    backgroundColor: '#DDD'
-  },
- 
-})
 
 export default App
