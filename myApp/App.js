043e7ae33e145e978f5a995e8a2debdcf6695562
davisantosr <davi-rocha2@hotmail.com>
2021-01-05 15:34:47 -0300

    AsyncStorage| add styling

--- myApp/App.js
+++ myApp/App.js
@@ -5,17 +5,71 @@ import {
   StyleSheet,
   TextInput, 
   TouchableOpacity,
-  
+
 } from 'react-native'
 
 export class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      input: '', 
+      name: 'Davi', 
+    }
+  }
   render() {
     return (
-      <View>
-        <Text></Text>
+      <View style={styles.container}>
+        <View style={styles.viewInput}>
+          <TextInput 
+            style={styles.input}
+            value={this.state.input}
+            onChangeText={text => this.setState({input: text})}
+            underlineColorAndroid='transparent'
+          />
+          <TouchableOpacity>
+            <Text style={styles.btn}>
+              +
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.name}>
+          {this.state.name}
+        </Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    marginTop: 20, 
+    alignItems: 'center'
+  },
+  viewInput: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    width: 350, 
+    height: 40, 
+    borderColor:'#000',
+    borderWidth: 1,
+    padding: 10
+  },
+  btn: {
+    backgroundColor: '#222', 
+    color: '#fff', 
+    height: 40,
+    padding: 10, 
+    marginLeft: 4
+  },
+  name: {
+    marginTop: 15,
+    fontSize: 30, 
+    textAlign: 'center'
+  }
+})
 
 export default App
