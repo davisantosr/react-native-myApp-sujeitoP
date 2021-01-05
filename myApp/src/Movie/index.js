ca2d23c3d5edaa098430a035db86f3b0db27d4e7
davisantosr <davi-rocha2@hotmail.com>
2021-01-05 17:14:21 -0300

    http requests|Movie| add styling

--- myApp/src/Movie/index.js
+++ myApp/src/Movie/index.js
@@ -1,17 +1,69 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
 
 export class Movie extends Component {
   render() {
-    const {nome} = this.props.data
+    const {nome, foto, } = this.props.data
     return (
       <View>
-        <Text>
-          {nome}
-        </Text>
+        <View style={styles.card}>
+          <Text style={styles.title}>
+            {nome}
+          </Text>
+          <Image 
+            style={styles.poster}
+            source={{uri: foto}}
+          />
+          <View style={styles.btnArea}>
+            <TouchableOpacity style={styles.btn} onPress={() => alert(nome)}>
+              <Text style={styles.btnText}>
+                READ MORE
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  card: {
+    shadowColor: '#000', 
+    backgroundColor: '#fff',
+    shadowOffset: {width: 0, height: 1},
+    shadowOpacity: 0.8, 
+    margin: 15,
+    shadowRadius: 5,
+    borderRadius: 5,
+    elevation: 3
+  },
+  title: {
+    fontSize: 18, 
+    padding: 15
+  },
+  poster: {
+    height: 250,
+    zIndex: 2,
+  },
+  btnArea: {
+    alignItems: 'flex-end',
+    marginTop: -40, 
+    zIndex: 9, 
+  },
+  btn: {
+    width: 100, 
+    backgroundColor: '#09a6FF',
+    opacity: 1, 
+    padding: 8,
+    borderTopLeftRadius: 5,
+    borderBottomLeftRadius: 5,
+  }, 
+  btnText: {
+    textAlign: 'center',
+    color: '#fff'
+  }
+
+})
 
 export default Movie
