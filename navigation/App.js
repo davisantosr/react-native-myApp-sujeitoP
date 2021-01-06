ae4228e49f1c78cb321ae61efc978811e77839c6
davisantosr <davi-rocha2@hotmail.com>
2021-01-06 10:25:40 -0300

    navigation|tab| screenOptions | tabBarIcon

--- navigation/App.js
+++ navigation/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 import Home from './src/pages/Home'
 import About from './src/pages/About'
@@ -8,10 +9,29 @@ import Contact from './src/pages/Contact'
 
 const Tab = createBottomTabNavigator()
 
+const icons = {
+  Home: {
+    name: 'ios-home'
+  },
+  About: {
+    name: 'ios-people'
+  },
+  Contact: {
+    name: 'ios-call'
+  }
+}
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({color, size}) => {
+            const {name} = icons[route.name]
+            return <Icon name={name} color={color} size={size} />
+          }
+        })}
+      >
         <Tab.Screen name={'Home'} component={Home} />
         <Tab.Screen name={'About'} component={About} />
         <Tab.Screen name={'Contact'} component={Contact} />
--- navigation/src/pages/Home/index.js
+++ navigation/src/pages/Home/index.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 
 function Home() {
+
+  const navigation = useNavigation()
+
   return (
     <View>
       <Text>
         Home
       </Text>
+      <Button 
+        title={'Contact'}
+        onPress={() => navigation.navigate('Contact')}
+      />
     </View>
   )
 }
