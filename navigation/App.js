76f5dac8ac319d50b49d611596af629cc8c33e6c
davisantosr <davi-rocha2@hotmail.com>
2021-01-06 10:52:04 -0300

    navigation | drawer navigation | createDrawerNavigator

--- navigation/App.js
+++ navigation/App.js
@@ -1,58 +1,22 @@
+import 'react-native-gesture-handler';
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { createStackNavigator } from '@react-navigation/stack'
-import Icon from 'react-native-vector-icons/Ionicons'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 
 import Home from './src/pages/Home'
 import About from './src/pages/About'
 import Contact from './src/pages/Contact'
 
-const Tab = createBottomTabNavigator()
-const Stack = createStackNavigator()
+const Drawer = createDrawerNavigator()
 
-const icons = {
-  Home: {
-    name: 'ios-home'
-  },
-  About: {
-    name: 'ios-people'
-  },
-  Contact: {
-    name: 'ios-call'
-  }
-}
-
-export function Tabs() {
+export default function App() {
   return (
-      <Tab.Navigator
-        screenOptions={({route}) => ({
-          tabBarIcon: ({color, size}) => {
-            const {name} = icons[route.name]
-            return <Icon name={name} color={color} size={size} />
-          }
-        })}
-        tabBarOptions={{
-          style:{
-            backgroundColor: '#121212'
-          },
-          activeTintColor: '#FFF',
-          inactiveTintColor: '#FF0000'
-        }}
-      >
-        <Tab.Screen name={'Home'} component={Home} />
-        <Tab.Screen name={'About'} component={About} />
-      </Tab.Navigator>
-  )
-}
-
-export default function App(){
-  return(
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name={'Home'} component={Tabs} options={{ headerShown: false}} />
-        <Stack.Screen name={'Contact'} component={Contact} />
-      </Stack.Navigator>
+      <Drawer.Navigator>
+        <Drawer.Screen name={'Home'} component={Home} />
+        <Drawer.Screen name={'About'} component={About} />
+        <Drawer.Screen name={'Contact'} component={Contact} />
+      </Drawer.Navigator>
     </NavigationContainer>
   )
 }
