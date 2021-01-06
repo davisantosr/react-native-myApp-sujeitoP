46d583261767e104108c20047fc2e107abefb49d
davisantosr <davi-rocha2@hotmail.com>
2021-01-06 11:12:42 -0300

    navigation|drawer|custom drawer| drawerContent prop

--- navigation/App.js
+++ navigation/App.js
@@ -7,12 +7,16 @@ import Home from './src/pages/Home'
 import About from './src/pages/About'
 import Contact from './src/pages/Contact'
 
+import CustomDrawer from './src/components/CustomDrawer'
+
 const Drawer = createDrawerNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
+      <Drawer.Navigator
+        drawerContent={CustomDrawer}
+      >
         <Drawer.Screen name={'Home'} component={Home} options={{headerShown: true}} />
         <Drawer.Screen name={'About'} component={About} />
         <Drawer.Screen name={'Contact'} component={Contact} />
