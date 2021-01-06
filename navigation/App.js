fcd7f8324fd16f2ccaeb64093285985804f506fa
davisantosr <davi-rocha2@hotmail.com>
2021-01-06 10:08:44 -0300

    navigation| tab |

--- navigation/App.js
+++ navigation/App.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import Home from './src/pages/Home'
 import About from './src/pages/About'
 import Contact from './src/pages/Contact'
 
-
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-
+      <Tab.Navigator>
+        <Tab.Screen name={'Home'} component={Home} />
+        <Tab.Screen name={'About'} component={About} />
+        <Tab.Screen name={'Contact'} component={Contact} />
+      </Tab.Navigator>
     </NavigationContainer>
   )
 }
